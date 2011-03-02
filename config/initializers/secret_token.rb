2e055a4e835014d808f8a606ecdc747a98728c5b
André Millet <andremillet@gmail.com>
2011-03-01 23:44:45 -0300

    ideias.ainda

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prognosis::Application.config.secret_token = '4d4475766cc9683fddbc3cbafd4b2e7ef199d6418cb25333c69eea9bce602f9679298f6cb11c048c9d3c9b38cae620e8419566747952c364bb99c5ad1bf50d3a'
